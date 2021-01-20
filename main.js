8cfe1c520cfe707208a402a6df07adcd3c334f04
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-01-19 19:42:46 -0600

    Reviewing arrays and array methods

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+const students = ['Katy', 'Jackie', 'Sean', 'Nathan'];
+// console.log(students.length);
+
+// ******** ACCESSING ITEMS IN ARRAY ********
+// console.log(students.pop());
+// console.log(students[3]); or console.log(students[students.length - 1]); to see what the last index of an array is.
+
+students[0] = "Matthew";
+// console.log(students)
+
+const valuePrinter = (array, index) => {
+  return array[index];
+};
+// console.log(valuePrinter(students, 2));
+
+const isNameHere = (array, name) => {
+ return (array.includes(name) ? `${name} is here` : `${name} is not here`);
+};
+
+console.log(isNameHere(students, 'Greg'));
+console.log(isNameHere(students, 'Sean'));
